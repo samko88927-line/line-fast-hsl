dbc194ee2ff5699d4970bf44666d986bc9755e68
Sam Ko <sam.ko@choco.media>
2024-07-18 17:22:51 +0800

    feat:auto change url

--- actions/get-video.tsx
+++ actions/get-video.tsx
@@ -1,9 +1,30 @@
 import { Product } from "@/types";
+const videoIdWeekend = "42";
+const videoIdWeekday = "60";
+const NEXT_PUBLIC_API_URL_PRO = "https://api.linetv.tw/v1";
+const NEXT_PUBLIC_API_URL = "https://cheetahmen.chocotv.com.tw/v1";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/fast/channels`;
-
-const getVideo = async (id: string): Promise<any> => {
-  const res = await fetch(`${URL}/${id}/info`);
+const getBaseUrlAndVideoId = () => {
+  const now = new Date();
+  const day = now.getUTCDay(); // 獲取當前星期幾，0 是星期日，6 是星期六
+  if (day >= 5 && day <= 0) {
+    // 星期五至星期日
+    return {
+      baseUrl: NEXT_PUBLIC_API_URL_PRO,
+      videoId: videoIdWeekend,
+    };
+  } else {
+    // 其餘時間
+    return {
+      baseUrl: NEXT_PUBLIC_API_URL,
+      videoId: videoIdWeekday,
+    };
+  }
+};
+// const URL = `${NEXT_PUBLIC_API_URL}/fast/channels`;
+const getVideo = async (): Promise<any> => {
+  const { baseUrl, videoId } = getBaseUrlAndVideoId();
+  const res = await fetch(`${baseUrl}/fast/channels/${videoId}/info`);
 
   return res.json();
 };
